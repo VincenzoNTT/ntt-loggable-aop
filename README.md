2e4407693c8b29381b220b24a8cf976255445813
Iamicellavi <vincenzo.iamicella@gmail.com>
2020-05-28 09:11:02 +0200

    readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# ntt-loggable-aop
+
+Java Method Logging with AOP and Annotations. - AOP with Spring Boot
+
+Lo scopo è quello di tracciare\loggare i tempi di esecuzioni dei java method. 
+
+### Dettagli implementazione 
+
+* Annotation with example metadata: [Loggable.java](https://github.com/VincenzoNTT/ntt-loggable-aop/blob/master/src/main/java/com/nttdata/aop/Loggable.java)
+* Implementation of different advice types: [LogInterceptor.java](https://github.com/VincenzoNTT/ntt-loggable-aop/blob/master/src/main/java/com/nttdata/aop/LogInterceptor.java)
+  
+ 
+ 
+## Project Example:
+Creato project maven di esempio con l'utilizzo di ntt-loggable-aop in [un altro repository](https://github.com/VincenzoNTT/ntt-loggable-aop-test)
--- src/main/java/com/nttdata/aop/LogInterceptor.java
+++ src/main/java/com/nttdata/aop/LogInterceptor.java
@@ -20,9 +20,13 @@ public class LogInterceptor {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(LogInterceptor.class);
 
+	/** */
+	final StopWatch stopWatch = new StopWatch();
+	
+	
 	@Before(value = "target(bean) && @annotation(Loggable) && @annotation(logme)", argNames = "bean,logme")
 	public void logBefore(JoinPoint jp, Object bean, Loggable logme) {
-
+		stopWatch.start();
 		LOGGER.debug(String.format("Log Message: %s", logme.message()));
 		LOGGER.debug(String.format("Method Called: %s", jp.getSignature().getName()));
 
@@ -32,36 +36,32 @@ public class LogInterceptor {
 	public void logAfter(JoinPoint jp, Object bean, Loggable logme) {
 
 		if (LOGGER.isDebugEnabled()) {
-			// LOGGER.debug("logAfter() is running!");
-			// LOGGER.debug(String.format("Method called: %s",
-			// jp.getSignature().getName()));
-			LOGGER.debug("Method terminated.");
+			stopWatch.stop();
+ 			LOGGER.debug("Method terminated - Execution time : " + stopWatch.getTotalTimeMillis() + " ms");
 			LOGGER.debug("******************************************************************************");
 		}
-
-	}
+ 	}
 
 	@Around(value = "@annotation(Loggable)")
 	public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
 		LOGGER.debug("******************************************************************************");
-		final StopWatch stopWatch = new StopWatch();
-		stopWatch.start();
 
 		if (LOGGER.isDebugEnabled()) {
 			LOGGER.debug("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
 					joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
 		}
+
 		try {
 			Object result = joinPoint.proceed();
-			stopWatch.stop();
-
+		
 			if (LOGGER.isDebugEnabled()) {
-				LOGGER.debug("Execution time : " + stopWatch.getTotalTimeMillis() + " ms");
 				LOGGER.debug("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
 						joinPoint.getSignature().getName(), result);
 			}
 			return result;
 		} catch (IllegalArgumentException e) {
+			stopWatch.stop();
+ 			LOGGER.debug("Method with error - Execution time : " + stopWatch.getTotalTimeMillis() + " ms");
 			LOGGER.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
 					joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
 			throw e;
